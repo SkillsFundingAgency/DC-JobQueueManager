5762522206538154c9b60a50efd8a819980643f7
Salman Baig <salman.sfa@outlook.com>
2018-05-04 12:04:52 +0100

    added script for job table

---
+++ src/DC.JobQueueManager.Database/Job.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE TABLE [dbo].[Job](
+	[JobId] [bigint] IDENTITY(1,1) NOT NULL,
+	[JobType] [int] NOT NULL,
+	[FileName] [varchar](50) NULL,
+	[Priority] [smallint] NOT NULL,
+	[DateTimeSubmittedUTC] [datetime] NOT NULL,
+	[DateTimeUpdatedUTC] [datetime] NULL,
+	[Ukprn] [bigint] NULL,
+	[StorageReference] [varchar](50) NULL,
+	[Status] [smallint] NOT NULL,
+	[Rowversion] [timestamp] NOT NULL,
+ CONSTRAINT [PK_Job_memoryoptimizedtable] PRIMARY KEY CLUSTERED 
+(
+	[JobId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
