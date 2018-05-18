e5e610ae5d160cb93ed759613c2594ca7c2346f0
Peter Chamley <peter@chamley.name>
2018-05-18 09:46:48 +0100

    Fix Rlease Published File - Add API Pwd Token

--- src/DC.JobQueueManager.Database/dbo/Stored Procedures/GetJobByPriority.sql
+++ src/DC.JobQueueManager.Database/dbo/Stored Procedures/GetJobByPriority.sql
@@ -26,7 +26,7 @@ BEGIN
 	WHERE [Status] = 1
 	AND NOT EXISTS (SELECT 1 FROM [dbo].[job] (nolock) 
 					WHERE [Status] IN (5,7) 
-					  And ( [JobType] = 2  Or ([JobType] =1 And [Ukprn] = j.[Ukprn] ) )
+					  And ( [JobType] = 2  Or ([JobType] =1 And [Ukprn] = j.[Ukprn]) )
 					)
 	ORDER BY [Priority] DESC, [JobId]
 
