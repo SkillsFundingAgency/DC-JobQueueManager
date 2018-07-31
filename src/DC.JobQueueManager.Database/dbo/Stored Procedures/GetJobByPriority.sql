157f8db7b67b3df55ca25bbd3c2d3c03b783bf4a
Salman Baig <salman.sfa@outlook.com>
2018-07-31 14:58:02 +0100

    added statuses which will wait to push next job for same ukprn

--- src/DC.JobQueueManager.Database/dbo/Stored Procedures/GetJobByPriority.sql
+++ src/DC.JobQueueManager.Database/dbo/Stored Procedures/GetJobByPriority.sql
@@ -24,7 +24,7 @@ BEGIN
 	FROM [dbo].[job] j WITH (nolock) 
 	WHERE [Status] = 1
 	AND NOT EXISTS (SELECT 1 FROM [dbo].[job] (nolock) 
-					WHERE [Status] IN (5,7) 
+					WHERE [Status] IN (2,3,8) 
 					  And ( [JobType] = 2  Or ([JobType] =1 And [Ukprn] = j.[Ukprn]) )
 					)
 	ORDER BY [Priority] DESC, [JobId]
