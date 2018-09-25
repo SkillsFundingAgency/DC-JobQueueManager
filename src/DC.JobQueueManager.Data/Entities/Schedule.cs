4a6f66d3415e1bd0afc6450dac6d201410b23624
John Youren <john.youren@gmail.com>
2018-09-25 10:00:17 +0100

    Added scheduler table for external reference data updates

---
+++ src/DC.JobQueueManager.Data/Entities/Schedule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESFA.DC.JobQueueManager.Data.Entities
+{
+    public class Schedule
+    {
+        [Key]
+        public long ID { get; set; }
+
+        [Required] public bool Enabled { get; set; }
+
+        public bool? MinuteIsCadence { get; set; }
+
+        public byte? Minute { get; set; }
+
+        public byte? Hour { get; set; }
+
+        public byte? DayOfTheMonth { get; set; }
+
+        public byte? Month { get; set; }
+
+        public byte? DayOfTheWeek { get; set; }
+
+        [Required] public byte ExternalDataType { get; set; }
+
+        [Required] public bool ExecuteOnceOnly { get; set; }
+
+        public DateTime? LastExecuteDateTime { get; set; }
+    }
+}
